7986c5954889db6876747b4bd5a27bf2e4cd2811
AlbertoCampos99 <camposalberto15@gmail.com>
2021-11-04 20:51:06 +0100

    first commit

---
+++ src/main/java/com/genuinegames/GenuineGamesApplication.java
@@ -0,0 +1,13 @@
+package com.genuinegames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenuineGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenuineGamesApplication.class, args);
+	}
+
+}
